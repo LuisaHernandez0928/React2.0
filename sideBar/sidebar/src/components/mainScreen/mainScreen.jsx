abf1d4701d09c56bade72c90a6c2eea9a0943426
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2023-12-22 09:18:17 -0500

    created main components and structure

---
+++ sideBar/sidebar/src/components/mainScreen/mainScreen.jsx
@@ -0,0 +1,11 @@
+import styles from './index.module.css'
+
+function MainScreen () {
+  return (
+    <div className={styles.container}>
+      Contenido
+    </div>
+  )
+}
+
+export { MainScreen } 
---
+++ sideBar/sidebar/src/components/sidebar/sidebar.jsx
@@ -0,0 +1,7 @@
+import styles from "./index.module.css";
+
+function Sidebar() {
+  return <div className={styles.container}>Hola Mundo</div>;
+}
+
+export { Sidebar };
