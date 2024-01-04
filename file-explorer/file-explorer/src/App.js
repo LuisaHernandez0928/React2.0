7b3a7ba112686ec1d5919ad9444dab043bc7b588
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-03 20:18:47 -0500

    created layout and sideBar states

--- file-explorer/file-explorer/src/App.js
+++ file-explorer/file-explorer/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { SidebarLayout } from "./layouts/sidebar";
+import { ContentView } from "./components/contentView";
+import { FolderView } from "./components/foldersView/foldersView";
+
+import "./App.css";
+
+const items = {
+  ejercicio: {
+    "package.json": "Hola",
+    "README.md": "mi",
+    public: {
+      "index.html": "hermosa",
+    },
+    src: {
+      "index.js": "lu",
+      "app.js": "te",
+      components: {
+        button: {
+          "index.js": "adoro",
+        },
+      },
+    },
+  },
+};
 
 function App() {
+  const [visibleContent, setVisibleContent] = useState("");
+  const [activeContextMenu, setActiveContextMenu] = useState(0);
+  const [startX, setStartX] = useState(0);
+  const [startY, setStartY] = useState(0);
+
+  const itemClickEvent = (content) => {
+    setVisibleContent(content);
+  };
+
+  const folderRightClickHandle = (startX, startY) => {
+    setStartX(startX);
+    setStartY(startY);
+    setActiveContextMenu(1);
+  };
+
+  const filesViewComponent = (
+    <FolderView
+      itemClick={itemClickEvent}
+      itemList={items}
+      folderRightClick={folderRightClickHandle}
+    />
+  );
+  const contentViewComponent = <ContentView content={visibleContent} />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      onClick={() => setActiveContextMenu(0)}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <SidebarLayout
+        sideBarWidth={"350px"}
+        sidebarElement={filesViewComponent}
+        contentElement={contentViewComponent}
+        startX={startX}
+        startY={startY}
+        activeContextMenu={activeContextMenu}
+      />
     </div>
   );
 }
