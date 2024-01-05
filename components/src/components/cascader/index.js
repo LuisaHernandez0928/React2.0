58d5bfb02f53eac6c60fc612a2b6e86ea772f08d
LuisaHernandez0928 <134203194+LuisaHernandez0928@users.noreply.github.com>
2024-01-04 21:41:18 -0500

    simplifying the logic of the cascader

--- components/src/components/cascader/index.js
+++ components/src/components/cascader/index.js
@@ -1,48 +1,35 @@
 import { useState } from "react";
-import styles from "./index.modules.css";
+import styles from "./index.module.css";
 
 const Cascader = ({ data }) => {
   const [selectedValues, setSelectedValues] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
+  const showOptions = () => {
+    setIsOpen(true);
+    console.log("si entró");
+  };
   const handleItemClick = (value) => {
     const newSelectedValues = [...selectedValues, value];
     setSelectedValues(newSelectedValues);
   };
 
-  const handleBackClick = () => {
-    const newSelectedValues = [...selectedValues];
-    newSelectedValues.pop();
-    setSelectedValues(newSelectedValues);
-  };
 
   const renderMenu = (menuData) => {
-    return (
-      <ul>
-        {menuData.map((item) => (
-          <li key={item.value} onClick={() => handleItemClick(item.value)}>
-            {item.label}
-            {item.children && renderMenu(item.children)}
-          </li>
-        ))}
-      </ul>
-    );
+    console.log("render1")
+    return menuData.map((car) => (
+      <option onClick={() => renderMenu(car.children)}>{car.label}</option>
+    ));
   };
 
   return (
-    <div>
-      {selectedValues.length > 0 && (
-        <button onClick={handleBackClick}>Atrás</button>
-      )}
-      {renderMenu(data)}
-      <div>
-        <strong>Selección actual:</strong>
-        {selectedValues.map((value, index) => (
-          <span key={value}>
-            {index > 0 && " > "}
-            {value}
-          </span>
-        ))}
-      </div>
+    <div className={styles.mainContainer}>
+      <select className={styles.dropDownMenu} onClick={() => showOptions()}>
+        <option value="" disabled selected>
+          Select Brand
+        </option>
+        {renderMenu(data)}
+      </select>
     </div>
   );
 };
